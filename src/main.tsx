9f6a84e80057c90538a2930df1e74c0b8cbef203
lbeul <l.beul@outlook.de>
2024-01-07 18:35:55 +0100

    feat: :sparkles: introduce react router globally

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from '../theme';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './router';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <ChakraProvider theme={theme}>
     <React.StrictMode>
-      <App />
+      <RouterProvider router={router} />
     </React.StrictMode>
   </ChakraProvider>
 );
